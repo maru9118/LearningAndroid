f60a4f347f5e1d9e6ebe087d229799705f8829fb
Demian Jaehoon Lim <jhlim@infraware.co.kr>
2017-12-10 15:44:54 +0900

    1234

--- app/src/main/java/com/overface/learningandroid/listview/MultiChoiceListViewActivity.java
+++ app/src/main/java/com/overface/learningandroid/listview/MultiChoiceListViewActivity.java
@@ -18,11 +18,11 @@ public class MultiChoiceListViewActivity extends AppCompatActivity {
 
     private void initLayout(){
         String[] items = getResources().getStringArray(R.array.arrays_list_item);
-        // 다중 체크 레이아웃 어뎁터 설정 android.R.layout.simple_list_item_single_choice
+        // 멀티 체크 레이아웃 어뎁터 설정 android.R.layout.simple_list_item_single_choice
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, items);
         ListView listview = findViewById(R.id.lv_multi);
 
-        // 다중 선택 설정
+        // 멀티 선택 설정
         listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         listview.setAdapter(adapter);
     }
